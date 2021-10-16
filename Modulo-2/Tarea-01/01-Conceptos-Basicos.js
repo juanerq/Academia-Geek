d354a50dce31f1cf0025fb04a0a45f86cfbb044c
juanerq <jrjuanreyes64@gmail.com>
2021-10-16 14:36:09 -0500

    Entrega-Modulo-2-01-Conceptos-Básicos-Tarea-01-Ejercicio-1

---
+++ Modulo-2/Tarea-01/01-Conceptos-Basicos.js
@@ -0,0 +1,32 @@
+//----------------------[ TAREA 1 ]--------------------------//
+/* Realizar un algoritmo en Javascript que permita calcular la suma, resta, multiplicación y división de dos números, donde num1 = 5 y num2 = 7. Mostrar los resultados por consola. */
+
+// Inicialización de variables
+let suma;
+let resta;
+let multiplicacion;
+let division;
+
+// Declaración de variables
+const num1 = 5;
+const num2 = 7;
+
+// Operacion con varibles
+// suma
+suma = num1 + num2;
+console.log('La suma de ' + num1 + ' + ' + num2 + ' es: ' + suma);
+
+// resta
+resta = num1 - num2;
+console.log('La resta de ' + num1 + ' - ' + num2 + ' es: ' + resta);
+
+// multiplicación
+multiplicacion = num1 * num2;
+console.log('La multiplicación de ' + num1 + ' x ' + num2 + ' es: ' + multiplicacion);
+
+// division
+division = num1 / num2;
+console.log('La división de ' + num1 + ' / ' + num2 + ' es: ' + division);
+
+
+
